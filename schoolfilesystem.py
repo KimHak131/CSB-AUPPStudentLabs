50f1e7ae3e7f975d1ba4309b1b9f1a134c6b17d4
KimHak131 <137867485+KimHak131@users.noreply.github.com>
2024-01-15 11:28:13 +0700

    update 1

--- schoolfilesystem.py
+++ schoolfilesystem.py
@@ -1,20 +1,43 @@
-
+import pandas as pd
 #Libraries you may need:
-import csv, collections, dictionary, (pandas as pd), urlopen, etc..
+# import csv, collections, dictionary, (pandas as pd), urlopen, etc..
 
 #classes and Functions to implement
 class SchoolAssessmentSystem:
-    def __init__():
+    def __init__(self):
+        self.data = None
+    def process_file(self, file_path): 
+        file_extension = file_path.split('.')[-1].lower()
+        
+        #Process CSV file
+        if file_extension == "csv":
+            self.data = pd.read_csv(file_path) 
 
-    def process_file():            
+        # Process Excel file
+        elif file_extension == ['xls', 'xlsx']:
+            self.data = pd.read_excel(file_path)
+            
+        # Process txt file
+        elif file_extension == "txt":
+            with open(file_path, "r") as file:
+                lines = file.readlines
+        else:
+            print(f"Unsupported file formate {file_extension}")
 
-    def transfer_data():
-
-    def fetch_web_data():
-
-    def analyze_content():
-
-    def generate_summary():
-
+    def transfer_data(self):
+        pass
+    def fetch_web_data(self):
+        pass
+    def analyze_content(self):
+        pass
+    def generate_summary(self):
+        pass
 
 # Analyze content & display result area
+# Create an instance of the SchoolAssessmentSystem class
+assessment_system = SchoolAssessmentSystem()
+
+# Call the process_file method on the instance as CSV file format
+assessment_system.process_file(r"D:/Spring Y2/Computer Science B/CSB-AUPPStudentLabs/data_107326465.csv")
+
+print(assessment_system.data)
